f208e644678ad1eaa259db2256ca993c8ae704f2
plguidez <pgvguidez@uwaterloo.ca>
2017-10-04 00:53:11 -0400

    Convert to React app

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+          <div id='stars'></div>
+          <div id='stars2'></div>
+          <div id='stars3'></div>
+          <div id='title'>
+              Brand spanking new version of the website coming soon!
+          </div>
+      </div>
+    );
+  }
+}
+
+export default App;
