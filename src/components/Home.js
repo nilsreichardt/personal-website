f19b619909e09745a4d63abbb98945807792466c
plguidez <guidezpl@users.noreply.github.com>
2017-10-20 11:36:04 -0400

    Create Home component

---
+++ src/components/Home.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Home.css'
+
+const Home = () => (
+    <div>
+        <div id='stars'></div>
+        <div id='stars2'></div>
+        <div id='stars3'></div>
+        <div id='title'>
+            Welcome to my website!
+            <p>More to come soon ↓</p>
+        </div>
+        <div style={{height: '2000px'}}>
+
+        </div>
+    </div>
+)
+
+export default Home;
