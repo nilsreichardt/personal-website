0df20ef91a7262a8f4611f3d406969eb381f6abd
guidezpl <guidezpl@users.noreply.github.com>
2018-05-31 11:56:28 -0400

    add scroll to top and slide/fade-in animations

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
+import ScrollToTop from './components/ScrollToTop';
 import PageNotFound from './components/PageNotFound';
-import Home from './components/Home'
-import NavBar from './components/NavBar'
-import Footer from './components/Footer'
-import About from './components/About'
-import Projects from './components/Projects'
+import Home from './components/Home';
+import NavBar from './components/NavBar';
+import Footer from './components/Footer';
+import About from './components/About';
+import Projects from './components/Projects';
 import './App.css';
-
-import arrow from './images/curved_arrow.png'
 
 const items = [
   {
@@ -35,15 +34,18 @@ const items = [
 class App extends Component {
   render = () => (
     <div className="App">
-      <NavBar items={items} />
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/about" component={About} />
-        <Route exact path="/projects" component={Projects} />
-        <Route exact path="/contact" render={() => <img src={arrow} alt='arrow' style={{position: 'absolute', left: '4%', top: '44%'}}/>} />
-        <Route component={PageNotFound} />
-      </Switch>
-      <Footer />
+      <ScrollToTop>
+        <NavBar items={items} />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/about" component={About} />
+          <Route exact path="/projects" component={Projects} />
+          <Route exact path="/contact" render={() => <div></div>}/>
+          <Route component={PageNotFound} />
+        </Switch>
+        <Footer />
+      </ScrollToTop>
+
     </div>
   )
 }
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,17 +2,30 @@ import React, { Component } from 'react';
 import './Footer.css';
 
 class Footer extends Component {
-    render() {
-        return (
-            <div className="FooterWrapper" ref={(el) => { this.contact = el; }}>
-                <ul>
-                    <li><a target="_blank" rel="noopener noreferrer" className="icon social" href="https://github.com/guidezpl"><i className="fa fa-github fa-3x"></i></a></li>
-                    <li><a target="_blank" rel="noopener noreferrer" className="icon social" href="https://linkedin.com/in/pierrelouisguidez"><i className="fa fa-linkedin fa-3x"></i></a></li>
-                    <li><a className="icon social" href="mailto:guidez.pl+inquiries@gmail.com"><i className="fa fa-envelope fa-3x"></i></a></li>
-                </ul>
-            </div>
-        )
-    }
+  render = () => (
+    <ul className="FooterWrapper">
+      <li>
+        <a target="_blank" rel="noopener noreferrer" className="icon social" href="https://github.com/guidezpl">
+          <i className="fa fa-github fa-2x"></i>
+        </a>
+      </li>
+      <li>
+        <a target="_blank" rel="noopener noreferrer" className="icon social" href="https://linkedin.com/in/pierrelouisguidez">
+          <i className="fa fa-linkedin fa-2x"></i>
+        </a>
+      </li>
+      <li>
+        <a className="icon social" href="mailto:guidez.pl+inquiries@gmail.com">
+          <i className="fa fa-envelope fa-2x"></i>
+        </a>
+      </li>
+      <li>
+        <a className="icon social" href="https://blog.guidez.pl">
+          <i className="fa fa-tumblr fa-2x"></i>
+        </a>
+      </li>
+    </ul>
+  )
 }
 
 export default Footer;
