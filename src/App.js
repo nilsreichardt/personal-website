c4d4460e832fac7c365241b1e054ba40dc8fffc0
guidezpl <guidezpl@users.noreply.github.com>
2018-05-30 23:09:19 -0400

    refactor navbar, darken bg colour on scroll

--- src/App.js
+++ src/App.js
@@ -5,25 +5,47 @@ import Home from './components/Home'
 import NavBar from './components/NavBar'
 import Footer from './components/Footer'
 import About from './components/About'
+import Projects from './components/Projects'
 import './App.css';
 
 import arrow from './images/curved_arrow.png'
 
+const items = [
+  {
+    'path': '/',
+    'title': 'Home',
+    'component': Home
+  },
+  {
+    'path': '/about',
+    'title': 'About',
+    'component': About
+  },
+  {
+    'path': '/projects',
+    'title': 'Projects',
+    'component': Projects
+  },
+  {
+    'path': '/contact',
+    'title': 'Contact',
+  },
+]
+
 class App extends Component {
-    render() {
-        return (
-            <div className="App">
-                <NavBar />
-                <Switch>
-                    <Route exact path="/" component={Home} />
-                    <Route exact path="/about" component={About} />
-                    <Route exact path="/contact" render={() => <img src={arrow} alt='arrow' style={{position: 'absolute', left: '4%', top: '44%'}}/>} />
-                    <Route component={PageNotFound} />
-                </Switch>
-                <Footer />
-            </div>
-        );
-    }
+  render = () => (
+    <div className="App">
+      <NavBar items={items} />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/projects" component={Projects} />
+        <Route exact path="/contact" render={() => <img src={arrow} alt='arrow' style={{position: 'absolute', left: '4%', top: '44%'}}/>} />
+        <Route component={PageNotFound} />
+      </Switch>
+      <Footer />
+    </div>
+  )
 }
 
 export default App;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,49 +4,35 @@ import { NavLink } from 'react-router-dom';
 import './NavBar.css';
 
 class NavBar extends Component {
-    state = {
-        isTop: window.scrollY === 0
-    }
+  state = {
+    isTop: window.scrollY === 0
+  }
 
-    handleScroll = () => {
-        let {isTop} = this.state
-        let topOfNav = this.nav.offsetTop;
-        // console.log(window.scrollY, topOfNav, isTop)
-        if (window.scrollY > topOfNav) {
-            if (isTop) {
-                // console.log('no longer at top')
-                this.setState({isTop:false})
-            }
-        } else {
-            if (!isTop) {
-                // console.log('now at top')
-                this.setState({isTop:true})
-            }
-        }
-    }
+  handleScroll = () => {
+    this.setState({isTop: !(window.scrollY > 0)})
+  }
 
-    componentDidMount() {
-        window.addEventListener('scroll', this.handleScroll);
-        this.nav = ReactDOM.findDOMNode(this);
-        this.handleScroll();
-    }
+  componentDidMount() {
+    window.addEventListener('scroll', this.handleScroll);
+    this.nav = ReactDOM.findDOMNode(this);
+    this.handleScroll();
+  }
 
-    componentWillUnmount() {
-        window.removeEventListener('scroll', this.handleScroll);
-    }
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.handleScroll);
+  }
 
-    render() {
-        return (
-            <nav className={"NavBar" + (this.state.isTop ? ' top' : '')}>
-                <div id="logo"></div>
-                <div id="links">
-                    <NavLink className="NavLink" exact to="/" activeClassName="is-active">Home</NavLink>
-                    <NavLink className="NavLink" to={{pathname: '/about'}} activeClassName="is-active">About</NavLink>
-                    <a className="NavLink" href="https://blog.guidez.pl">Blog</a>
-                    <NavLink className="NavLink" to={{pathname: '/contact'}} activeClassName="is-active">Contact</NavLink>
-                </div>
-            </nav>
-        )
-    }
-}
-export default NavBar;
+  render = () => (
+    <nav className={"NavBar" + (this.state.isTop ? ' top' : '')}>
+      <div id="logo"></div>
+      <div id="links">
+        {
+          this.props.items.map((item) =>
+          <NavLink className="NavLink" key={item.title} exact activeClassName="is-active" to={`${item.path}`}>{item.title}</NavLink>)
+          }
+          <a className="NavLink" href="https://blog.guidez.pl">Blog</a>
+        </div>
+      </nav>
+    )
+  }
+  export default NavBar;
