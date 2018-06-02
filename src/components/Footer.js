7c269ef95837c377c95e8d235a2dfa0b9fa0c731
guidezpl <guidezpl@users.noreply.github.com>
2018-06-02 19:41:30 -0400

    fix accessibility and seo issues

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,22 +5,22 @@ class Footer extends Component {
   render = () => (
     <ul className="FooterWrapper">
       <li>
-        <a target="_blank" rel="noopener noreferrer" className="icon social" href="https://github.com/guidezpl">
+        <a target="_blank" rel="noopener noreferrer" className="icon social" href="https://github.com/guidezpl" title="github">
           <i className="fa fa-github fa-2x"></i>
         </a>
       </li>
       <li>
-        <a target="_blank" rel="noopener noreferrer" className="icon social" href="https://linkedin.com/in/pierrelouisguidez">
+        <a target="_blank" rel="noopener noreferrer" className="icon social" href="https://linkedin.com/in/pierrelouisguidez" title="linkedin">
           <i className="fa fa-linkedin fa-2x"></i>
         </a>
       </li>
       <li>
-        <a className="icon social" href="mailto:guidez.pl+inquiries@gmail.com">
+        <a className="icon social" href="mailto:guidez.pl+inquiries@gmail.com" title="email">
           <i className="fa fa-envelope fa-2x"></i>
         </a>
       </li>
       <li>
-        <a className="icon social" href="https://blog.guidez.pl">
+        <a className="icon social" href="https://blog.guidez.pl" title="tumblr">
           <i className="fa fa-tumblr fa-2x"></i>
         </a>
       </li>
